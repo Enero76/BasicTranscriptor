2472fa8a8182efd9a83196073e2f744148df317a
Enero76 <jantolin@gmail.com>
2026-05-20 23:23:01 +0200

    Added button to write the transcription to a text file

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 Basic Streamlit interface for a whisper based transcriptor
 
 ## Create the Conda environment:
+### Directly using requirements.txt
+conda create -n VOICE python=3.11
+
+pip install -r requirements.txt
+### Using the conda
 conda env create -f VOICE.yml
 
 ## Execute the app:
--- app_transcript.py
+++ app_transcript.py
@@ -2,9 +2,16 @@ import streamlit as st
 import whisper
 import tempfile
 
+from tkinter import Tk
+from tkinter.filedialog import asksaveasfilename
+
+
 st.title("Whisper Transcription")
 
 model = whisper.load_model("base")
+
+if "text" not in st.session_state:
+    st.session_state.text = ""
 
 audio_file = st.file_uploader("Upload an audio file to transcribe", type=["wav", "mp3", "m4a"])
 
@@ -18,3 +25,9 @@ if audio_file:
     if st.button("Transcribe"):
         result = model.transcribe(tmp_path)
         st.text_area("Text", result["text"], height=200)
+        st.session_state.text = result["text"]
+
+    if st.button("Export to Text File"):
+        with open("transcription.txt", "w") as f:
+            f.write(st.session_state.text)
+        st.success("Transcription saved to transcription.txt")
